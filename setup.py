301b17c9cab1a34ddbe21fd75e627497e253ea81
Pedro Algarvio <pedro@algarvio.me>
2024-02-22 17:32:41 +0000

    Mirror: 0.9.0a1

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ from setuptools import setup
 setup(
     name='pre_commit_placeholder_package',
     version='0.0.0',
-    install_requires=['poetry==0.9.0a0'],
+    install_requires=['poetry==0.9.0a1'],
 )
